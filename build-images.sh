ca8625464d5818752c4caa9c11c8dd7a34380b3d
Stephane de Labrusse <stephdl@de-labrusse.fr>
2022-11-25 15:12:41 +0100

    Fix the node build issue

--- build-images.sh
+++ build-images.sh
@@ -16,11 +16,11 @@ container=$(buildah from scratch)
 # Reuse existing nodebuilder-code container, to speed up builds
 if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-code; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-code -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-code -v "${PWD}:/usr/src:Z" docker.io/library/node:18-slim
 fi
 
 echo "Build static UI files with node..."
-buildah run nodebuilder-code sh -c "cd /usr/src/ui && yarn install && yarn build"
+buildah run --env="NODE_OPTIONS=--openssl-legacy-provider" nodebuilder-code sh -c "cd /usr/src/ui && yarn install && yarn build"
 
 # Add imageroot directory to the container image
 buildah add "${container}" imageroot /imageroot
