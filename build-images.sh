b7a2c7b719ec9ba35a361605d640cda43b3a976c
Stephane de Labrusse <stephdl@de-labrusse.fr>
2023-11-13 16:25:33 +0100

    Upgrade collabora to 23.05.5.4.1

--- build-images.sh
+++ build-images.sh
@@ -30,7 +30,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/collabora/code:21.11.5.3.1" \
+    --label="org.nethserver.images=docker.io/collabora/code:23.05.5.4.1" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
